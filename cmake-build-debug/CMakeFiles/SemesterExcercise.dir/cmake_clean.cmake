8a0debf645b03ab7f84f5f5ae7285375d37300cf
DKR <iamthedkr@gmail.com>
2019-10-08 23:09:00 +0700

    Update workspace Clion

---
+++ cmake-build-debug/CMakeFiles/SemesterExcercise.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai1.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai2.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai3.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai4.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai5.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai6.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai7.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai8.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/Functions/Bai9.cpp.obj"
+  "CMakeFiles/SemesterExcercise.dir/main.cpp.obj"
+  "SemesterExcercise.exe"
+  "SemesterExcercise.exe.manifest"
+  "SemesterExcercise.pdb"
+  "libSemesterExcercise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SemesterExcercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
